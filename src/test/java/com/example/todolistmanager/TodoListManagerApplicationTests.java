524bab42a26e0b068a2f6175aab936000ed8cd04
jenny-min <jennynguyen.92.92@gmail.com>
2026-05-31 08:41:07 +0900

    create todolist project,add  Dependencies and start project with JPA DB successful

---
+++ src/test/java/com/example/todolistmanager/TodoListManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.todolistmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoListManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
